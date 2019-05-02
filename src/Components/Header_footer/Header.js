1e60e417ccadd23d91889e02e3405edb373d438e
zavion sherman <zsherman2510@hotmail.com>
2019-05-01 21:13:41 -0700

    initial commit

---
+++ src/Components/Header_footer/Header.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import { CityLogo } from "../utils/icons";
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <AppBar
+          position="fixed"
+          style={{
+            backgroundColor: "#98c5e9",
+            boxShadow: "none",
+            padding: "10px 0",
+            borderBottom: "2px solid #002e5e"
+          }}
+        >
+          <Toolbar styles={{ display: "flex" }}>
+            <div styles={{ flexGrow: 1 }}>
+              <div className="header_logo">
+                <CityLogo link={true} linkTo="/" width="70px" height="70px" />
+              </div>
+            </div>
+            <Link to="/the_team">
+              <Button color="inherit">The Team</Button>
+            </Link>
+            <Link to="/the_matches">
+              <Button color="inherit">The Matches</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </>
+    );
+  }
+}
+
+export default Header;
---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "./HOC/Layout";
+const Routes = props => {
+  return (
+    <Layout>
+      <h1>children</h1>
+    </Layout>
+  );
+};
+
+export default Routes;
